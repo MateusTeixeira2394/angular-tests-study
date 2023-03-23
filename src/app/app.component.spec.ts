b4a6581458ec876f234b46140c182c53dcd05732
Mateus Teixeira <mateus.teixeira2394@gmail.com>
2023-03-23 16:00:44 -0300

    Added app component tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 import {LikeWidgetComponent} from './components/like-widget/like-widget.component'
 
-describe('AppComponent', () => {
+describe(`${AppComponent.name}`, () => {
+
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
+
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
@@ -16,24 +21,27 @@ describe('AppComponent', () => {
         LikeWidgetComponent
       ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('Should be the created', () => {
+    expect(component).toBeTruthy();
   });
 
-  // it(`should have as title 'study-tests-study'`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('study-tests-study');
-  // });
+  it(`it should increment the likes whenever the method ${AppComponent.prototype.like.name} is called`, ()=>{
 
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('.content span')?.textContent).toContain('study-tests-study app is running!');
-  // });
+    const times: number = 50;
+
+    for (let i = 0; i < times; i++) {
+      component.like();
+    };
+
+    expect(component.likes).toBe(50);
+
+  });
+
 });
